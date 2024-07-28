de5f9699ced1ff3e05babf9684f9e1de001f5fe8
Picalines <picalines0@gmail.com>
2024-07-28 21:41:39 +0300

    feat(ui-kit): basic button

---
+++ packages/ui-kit/src/lib/classnames.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function tw(strings: readonly string[] | ArrayLike<string>, ...values: readonly unknown[]) {
+  return String.raw({ raw: strings }, ...values)
+}
