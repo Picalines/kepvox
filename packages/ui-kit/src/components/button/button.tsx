048bfd4547c854d2edbab6501e73dc517c9d90f0
picalines <picalines0@gmail.com>
2024-08-04 02:36:21 +0300

    fix(ui-kit): remove interface

--- packages/ui-kit/src/components/button/button.tsx
+++ packages/ui-kit/src/components/button/button.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/classnames'
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps, cva } from 'class-variance-authority'
-import { forwardRef } from 'react'
+import { type ComponentProps, forwardRef } from 'react'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -28,11 +28,10 @@ const buttonVariants = cva(
   },
 )
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
-}
+export type ButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
