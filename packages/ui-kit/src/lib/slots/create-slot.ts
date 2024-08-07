9004e36380b3d521ca82366740e3701438e00298
picalines <picalines0@gmail.com>
2024-08-08 00:40:11 +0300

    chore(ui-kit): auto infer ref type from props in createSlot

--- packages/ui-kit/src/lib/slots/create-slot.ts
+++ packages/ui-kit/src/lib/slots/create-slot.ts
@@ -1,6 +1,9 @@
+import type { RefAttributes } from 'react'
 import type { SlotComponent } from './slot'
 
-export function createSlot<Props = {}, Type = never>(name: string): SlotComponent<Props, Type> {
+type RefType<Props> = Props extends RefAttributes<infer T> ? (unknown extends T ? never : T) : never
+
+export function createSlot<Props = {}, Type = RefType<Props>>(name: string): SlotComponent<Props, Type> {
   const Slot: SlotComponent<Props, Type> = () => null
 
   Slot.displayName = `Slot(${name})`
