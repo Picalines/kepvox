308903b4bf912790877abb14ff58563346083520
picalines <picalines0@gmail.com>
2024-08-09 17:24:56 +0300

    feat(ui-kit): Toggle component

---
+++ packages/ui-kit/src/components/toggle/toggle.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+
+import { Toggle, type ToggleProps } from './toggle'
+
+type StoryArgs = ToggleProps
+
+export default {
+  title: 'components/Toggle',
+  component: Toggle,
+  args: {
+    onClick: fn(),
+    onToggle: fn(),
+  },
+  argTypes: {
+    size: {
+      control: 'select',
+    },
+    onClick: {
+      table: { disable: true },
+    },
+    onToggle: {
+      table: { disable: true },
+    },
+    asChild: {
+      table: { disable: true },
+    },
+  },
+} satisfies Meta<StoryArgs>
+
+type Story = StoryObj<StoryArgs>
+
+export const Default: Story = {
+  args: {
+    children: 'Toggle',
+    variant: 'default',
+    size: 'default',
+  },
+}
---
+++ packages/ui-kit/src/components/toggle/toggle.tsx
@@ -0,0 +1,44 @@
+import { cn } from '@/lib/classnames'
+import * as RadixToggle from '@radix-ui/react-toggle'
+import { type VariantProps, cva } from 'class-variance-authority'
+import { type ComponentProps, forwardRef } from 'react'
+
+const toggleVariants = cva(
+  'inline-flex items-center justify-center rounded-md font-medium text-sm ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:text-accent-foreground',
+  {
+    variants: {
+      variant: {
+        default: 'bg-transparent',
+        outline: 'border border-input bg-transparent hover:bg-accent hover:text-accent-foreground',
+      },
+      size: {
+        default: 'h-10 px-3',
+        sm: 'h-9 px-2.5',
+        lg: 'h-11 px-5',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+)
+
+type ToggleProps = ComponentProps<'button'> &
+  VariantProps<typeof toggleVariants> & {
+    onToggle?: (pressed: boolean) => void
+    asChild?: boolean
+  }
+
+const Toggle = forwardRef<HTMLButtonElement, ToggleProps>(({ className, variant, size, onToggle, ...props }, ref) => (
+  <RadixToggle.Root
+    {...props}
+    ref={ref}
+    className={cn(toggleVariants({ variant, size, className }))}
+    onPressedChange={onToggle}
+  />
+))
+
+Toggle.displayName = 'Toggle'
+
+export { Toggle, toggleVariants, type ToggleProps }
