b892cb727d9681ab35cba0420c64b3820e122a56
picalines <picalines0@gmail.com>
2024-08-09 17:46:47 +0300

    doc(ui-kit): show icons in storybook

---
+++ packages/ui-kit/src/icons/icons.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Tooltip } from '@/components/tooltip'
+import * as icons from './index'
+
+export default {
+  title: 'icons/All',
+
+  render: () => (
+    <Tooltip.Provider delayDuration={0}>
+      <div className="flex flex-wrap gap-2">
+        {Object.entries(icons).map(([name, Icon]) => (
+          <Tooltip key={name}>
+            <Tooltip.Trigger asChild>
+              <div className="rounded-md border border-dashed p-1">
+                <Icon size={40} />
+              </div>
+            </Tooltip.Trigger>
+            <Tooltip.Content>{name.replace('Icon', '')}</Tooltip.Content>
+          </Tooltip>
+        ))}
+      </div>
+    </Tooltip.Provider>
+  ),
+} satisfies Meta
+
+type Story = StoryObj
+
+export const Default: Story = {}
