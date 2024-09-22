76ce4a95ee3d9ada24634dfaef68d07c1b12360d
Picalines <picalines0@gmail.com>
2024-09-22 19:51:58 +0300

    refactor(ui-kit): Card.Header is a simple slot

--- packages/ui-kit/src/components/card/card.stories.tsx
+++ packages/ui-kit/src/components/card/card.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { cn } from '@/lib/classnames'
+import { Heading } from '../heading'
 import { Card, type CardProps } from './card'
 
 type StoryArgs = CardProps & Partial<{ title: string; description: string; content: string; footer: string }>
@@ -10,10 +11,14 @@ export default {
   component: Card,
   render: ({ className, title, description, content, footer, ...args }) => (
     <Card {...args} className={cn('max-w-[400px]', className)}>
-      <Card.Header>
-        {title ? <Card.Header.Title>{title}</Card.Header.Title> : null}
-        {description ? <Card.Header.Description>{description}</Card.Header.Description> : null}
-      </Card.Header>
+      {title || description ? (
+        <Card.Header>
+          <Heading>
+            <Heading.Title>{title}</Heading.Title>
+            <Heading.Description color="secondary">{description}</Heading.Description>
+          </Heading>
+        </Card.Header>
+      ) : null}
       {content ? <Card.Content>{content}</Card.Content> : null}
       {footer ? <Card.Footer>{footer}</Card.Footer> : null}
     </Card>
--- packages/ui-kit/src/components/card/card.tsx
+++ packages/ui-kit/src/components/card/card.tsx
@@ -4,51 +4,12 @@ import { type ComponentProps, forwardRef } from 'react'
 
 type RootProps = ComponentProps<'div'>
 type HeaderProps = ComponentProps<'div'>
-type TitleProps = ComponentProps<'h3'>
-type DescriptionProps = ComponentProps<'p'>
 type ContentProps = ComponentProps<'div'>
 type FooterProps = ComponentProps<'div'>
 
 const HeaderSlot = createSlot<HeaderProps>('Header')
-const TitleSlot = createSlot<TitleProps>('Header.Title')
-const DescriptionSlot = createSlot<DescriptionProps>('Header.Description')
 const ContentSlot = createSlot<ContentProps>('Content')
 const FooterSlot = createSlot<FooterProps>('Footer')
-
-const Header = forwardRef<HTMLDivElement, HeaderProps>(({ className, children, ...props }, ref) => {
-  const slots = useSlots({ children, defaultSlot: TitleSlot })
-
-  const title = slots.get(TitleSlot)
-  const description = slots.get(DescriptionSlot)
-
-  if (!title && !description) {
-    return <div className={cn('mt-6', className)} />
-  }
-
-  return (
-    <div {...props} ref={ref} className={cn('flex flex-col space-y-1.5 p-6', className)}>
-      {title && (
-        <h3
-          {...title.props}
-          ref={title.ref}
-          className={cn('font-semibold text-2xl leading-none tracking-tight', title.props.className)}
-        >
-          {title.children}
-        </h3>
-      )}
-
-      {description && (
-        <p
-          {...description.props}
-          ref={description.ref}
-          className={cn('text-muted-foreground text-sm', description.props.className)}
-        >
-          {description.children}
-        </p>
-      )}
-    </div>
-  )
-})
 
 const Root = forwardRef<HTMLDivElement, RootProps>(({ className, children, ...props }, ref) => {
   const slots = useSlots({ children, defaultSlot: ContentSlot })
@@ -57,26 +18,33 @@ const Root = forwardRef<HTMLDivElement, RootProps>(({ className, children, ...pr
   const content = slots.get(ContentSlot)
   const footer = slots.get(FooterSlot)
 
+  if (!header && !content && !footer) {
+    return null
+  }
+
   return (
     <div
       ref={ref}
-      className={cn('rounded-lg border border-border bg-card text-card-foreground shadow-sm', className)}
+      className={cn(
+        'flex flex-col gap-3 rounded-lg border border-border bg-card p-3 text-card-foreground shadow-sm',
+        className,
+      )}
       {...props}
     >
       {header && (
-        <Header {...header.props} ref={header.ref}>
+        <div {...header.props} ref={header.ref}>
           {header.children}
-        </Header>
+        </div>
       )}
 
       {content && (
-        <div {...content.props} ref={content.ref} className={cn('p-6 pt-0', content.props.className)}>
+        <div {...content.props} ref={content.ref}>
           {content.children}
         </div>
       )}
 
       {footer && (
-        <div {...footer.props} ref={footer.ref} className={cn('flex items-center p-6 pt-0', footer.props.className)}>
+        <div {...footer.props} ref={footer.ref}>
           {footer.children}
         </div>
       )}
@@ -85,10 +53,7 @@ const Root = forwardRef<HTMLDivElement, RootProps>(({ className, children, ...pr
 })
 
 const Card = Object.assign(Root, {
-  Header: Object.assign(HeaderSlot, {
-    Title: TitleSlot,
-    Description: DescriptionSlot,
-  }),
+  Header: HeaderSlot,
   Content: ContentSlot,
   Footer: FooterSlot,
 })
@@ -99,8 +64,6 @@ export {
   Card,
   type RootProps as CardProps,
   type HeaderProps as CardHeaderProps,
-  type TitleProps as CardTitleProps,
-  type DescriptionProps as CardDescriptionProps,
   type ContentProps as CardContentProps,
   type FooterProps as CardFooterProps,
 }
