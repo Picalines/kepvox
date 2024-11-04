d52335671ed78129f69c6d282992fb5e78d71201
Picalines <picalines0@gmail.com>
2024-11-04 15:28:33 +0300

    feat(sc-js): ScClient class connecting to scsynth

---
+++ packages/sc-js/src/main/scala/scjs/ScClient.scala
@@ -0,0 +1,62 @@
+package scjs
+
+import de.sciss.osc
+import de.sciss.synth.{Client, Server, ServerConnection}
+
+import java.util.{Timer, TimerTask}
+import scala.annotation.unused
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Promise
+import scala.language.postfixOps
+import scala.scalajs.js
+import scala.scalajs.js.JSConverters._
+import scala.scalajs.js.annotation._
+import scala.scalajs.js.|
+import scala.util.chaining.scalaUtilChainingOps
+
+@JSExportTopLevel("ScClient")
+class ScClient(private val server: Server) extends js.Object {
+  @unused
+  def quit(): Unit = server.quit()
+}
+
+private object ScClient {
+  private val timer: Timer = new Timer(true)
+
+  @js.native
+  trait ConnectionConfig extends js.Object {
+    val port: Int = js.native
+    val timeout: js.UndefOr[Int] = js.native
+  }
+
+  @unused
+  @JSExportStatic
+  def connect(config: ConnectionConfig): js.Promise[ScClient | Null] = {
+    val clientConfig = Client.Config()
+    val serverConfig = Server.Config().tap { it =>
+      it.transport = osc.Browser
+      it.port = config.port
+    }
+
+    val promise = Promise[ScClient | Null]()
+    val future = promise.future
+
+    val connection = Server.connect("browser", serverConfig, clientConfig) {
+      case ServerConnection.Running(server) =>
+        promise.success(new ScClient(server))
+      case ServerConnection.Aborted =>
+        promise.success(null)
+    }
+
+    config.timeout.foreach { timeoutMillis =>
+      val timeoutTask = new TimerTask {
+        override def run(): Unit = connection.abort()
+      }
+
+      timer.schedule(timeoutTask, timeoutMillis)
+      future.onComplete { _ => timeoutTask.cancel() }
+    }
+
+    future.toJSPromise
+  }
+}
