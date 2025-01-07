a478bf6d9074baadabee257215cba192400b31b5
Picalines <picalines0@gmail.com>
2025-01-08 00:12:14 +0300

    test(common): type check brands

---
+++ packages/common/src/branded/branded.spec-d.ts
@@ -0,0 +1,17 @@
+import type { Branded } from '.'
+
+it('separates the inner type', () => {
+  expectTypeOf<Branded<number, 'brand'>>().not.toEqualTypeOf<number>()
+})
+
+it("doesn't separate the same brands", () => {
+  expectTypeOf<Branded<number, 'brand'>>().toEqualTypeOf<Branded<number, 'brand'>>()
+})
+
+it('separates the different brands', () => {
+  expectTypeOf<Branded<number, '1'>>().not.toEqualTypeOf<Branded<number, '2'>>()
+})
+
+it('not allows the literal value', () => {
+  expectTypeOf(123).not.toMatchTypeOf<Branded<number, 'brand'>>()
+})
