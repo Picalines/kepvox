326d3ec2588bccffefe24a953d62bff8811e2c03
Picalines <picalines0@gmail.com>
2025-01-08 16:52:02 +0300

    feat(common): overlay type

--- packages/common/src/typing.ts
+++ packages/common/src/typing.ts
@@ -1,1 +1,15 @@
-export type OmitExisting<T, K extends keyof T> = Omit<T, K>
+/**
+ * Like standard `Omit`, but requires the keys to be in `T`
+ */
+export type OmitExisting<T, K extends keyof T> = { [P in keyof T as P extends K ? never : P]: T[P] }
+
+export type Override<T extends object, O extends Partial<Record<keyof T, any>>> = {
+  [K in keyof T as K extends keyof O ? never : K]: T[K]
+} & O
+
+/**
+ * Like `Override`, but allows to add additional properties
+ */
+export type Overlay<T extends object, O extends Partial<Record<keyof T, any>> & object> = {
+  [K in keyof T as K extends keyof O ? never : K]: T[K]
+} & O
