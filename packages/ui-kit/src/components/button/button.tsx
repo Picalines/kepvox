a34d3a482867b60ba063e5d6e4d84280ff9873d4
Picalines <picalines0@gmail.com>
2025-01-09 23:08:46 +0300

    feat(ui-kit): ring-ring by default

--- packages/ui-kit/src/components/button/button.tsx
+++ packages/ui-kit/src/components/button/button.tsx
@@ -4,7 +4,7 @@ import { type ComponentProps, forwardRef } from 'react'
 import { cn } from '#lib/classnames'
 
 export const buttonVariants = cva(
-  'relative inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'relative inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
--- packages/ui-kit/src/components/dialog/dialog.tsx
+++ packages/ui-kit/src/components/dialog/dialog.tsx
@@ -101,7 +101,7 @@ export const Root: FC<RootProps> = props => {
             )}
             {content.children}
             {closable ? (
-              <RadixDialog.Close className="absolute top-4 right-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+              <RadixDialog.Close className="absolute top-4 right-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
                 <XIcon className="h-4 w-4" />
                 <span className="sr-only">Close</span>
               </RadixDialog.Close>
