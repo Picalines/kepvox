f72baf93d820cd2c7993cbf58e7cd4aaa82f2bdb
Picalines <picalines0@gmail.com>
2025-01-12 23:58:44 +0300

    feat(common): Tuple

--- packages/common/src/array/array-predicate.ts
+++ packages/common/src/array/array-predicate.ts
@@ -1,3 +1,12 @@
+import type { Tuple } from '#typing'
+
 export const isNonEmpty = <T>(array: T[]): array is [T, ...T[]] => {
   return array.length > 0
+}
+
+export const isTuple = <T, const L extends number>(
+  array: T[],
+  length: L,
+): array is number extends L ? T[] : Tuple<T, L> => {
+  return array.length === length
 }
