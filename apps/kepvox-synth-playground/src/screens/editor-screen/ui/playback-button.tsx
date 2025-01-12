20930df5af8958056d135189fa445a7d7c53166c
Picalines <picalines0@gmail.com>
2025-01-12 17:11:26 +0300

    feat(playground): simple example on play button
    Proper playground in #9

--- apps/kepvox-synth-playground/src/screens/editor-screen/ui/playback-button.tsx
+++ apps/kepvox-synth-playground/src/screens/editor-screen/ui/playback-button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { OscillatorSynthNode, OutputSynthNode, SynthContext } from '@repo/synth'
+import { waitUntil } from '@repo/common/promise'
+import { ADSREnvelopeSynthNode, OscillatorSynthNode, OutputSynthNode, SynthContext } from '@repo/synth'
 import { Button } from '@repo/ui-kit/components/button'
 import { PlayIcon } from '@repo/ui-kit/icons'
 
@@ -12,11 +13,29 @@ export const PlaybackButton = () => {
     const synthContext = new SynthContext(audioContext)
 
     const oscillator = new OscillatorSynthNode(synthContext)
+    const envelope = new ADSREnvelopeSynthNode(synthContext)
     const output = OutputSynthNode.ofContext(synthContext)
-    oscillator.connectOutput(output)
+
+    oscillator.connectOutput(envelope)
+    envelope.connectOutput(output)
+
+    await waitUntil(100, () => audioContext.state === 'running')
+
+    // biome-ignore lint/suspicious/noConsoleLog: temporary
+    console.log('running!')
+
+    oscillator.frequency.setImmediate(880)
+    oscillator.waveShape.setImmediate('sawtooth')
+    envelope.attack.setImmediate(0.1)
+    envelope.release.setImmediate(0.5)
 
     // @ts-expect-error
+    window.e = envelope
+    // @ts-expect-error
     window.o = oscillator
+
+    envelope.attackAt({ fromAhead: true })
+    envelope.releaseAt({ fromAhead: true, seconds: 0.2 })
   }
 
   return (
