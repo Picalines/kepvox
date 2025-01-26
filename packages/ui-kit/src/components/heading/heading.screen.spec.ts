dd3bf15575c5f30983b4eaa5b822e9911d905cd4
Picalines <picalines0@gmail.com>
2025-01-26 18:07:20 +0300

    feat(ui-kit): heading align

--- packages/ui-kit/src/components/heading/heading.screen.spec.ts
+++ packages/ui-kit/src/components/heading/heading.screen.spec.ts
@@ -3,6 +3,6 @@ import meta from './heading.stories'
 
 testStoryMatrix({
   meta,
-  stories: ['Default', 'NoSuperTitle', 'NoDescription', 'NoTitle'],
+  stories: ['Default', 'Center', 'Right', 'NoSuperTitle', 'NoDescription', 'NoTitle'],
   themes: ['light', 'dark'],
 })
