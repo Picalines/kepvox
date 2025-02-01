10a0af0310ab63ff92a10349331453285ee59d8c
Picalines <picalines0@gmail.com>
2025-02-01 23:27:14 +0300

    refactor(synth): SynthContext.output

--- packages/synth/src/node/output-synth-node.ts
+++ packages/synth/src/node/output-synth-node.ts
@@ -5,21 +5,12 @@ import { SynthNode, synthNodeType } from './synth-node'
 export class OutputSynthNode extends SynthNode {
   readonly [synthNodeType] = 'output'
 
-  private constructor(context: SynthContext) {
+  /**
+   * NOTE: you should get the instance from {@link SynthContext.output}
+   */
+  constructor(context: SynthContext) {
     const { destination } = context[INTERNAL_AUDIO_CONTEXT]
 
     super({ context, inputs: [destination], outputs: [] })
-  }
-
-  static readonly #contextOutputs = new WeakMap<SynthContext, OutputSynthNode>()
-
-  static ofContext(context: SynthContext) {
-    const output = OutputSynthNode.#contextOutputs.get(context) ?? new OutputSynthNode(context)
-
-    if (!OutputSynthNode.#contextOutputs.has(context)) {
-      OutputSynthNode.#contextOutputs.set(context, output)
-    }
-
-    return output
   }
 }
