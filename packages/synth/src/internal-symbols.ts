b66f31b71078a07e60dd434e4e1edcbcc279b0ad
Picalines <picalines0@gmail.com>
2025-02-08 21:32:16 +0300

    refactor(synth): remove own internal symbol

--- packages/synth/src/internal-symbols.ts
+++ packages/synth/src/internal-symbols.ts
@@ -1,7 +1,5 @@
 // NOTE: these symbols are meant to be used only inside the synth package
 
-export const INTERNAL_CONTEXT_OWN: unique symbol = Symbol('SynthContext.internal.own')
-
 /**
  * The "design" is to make the package (at least the most part of its API) abstracted from the Web Audio API
  */
