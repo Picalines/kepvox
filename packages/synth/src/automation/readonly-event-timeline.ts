2e05cc4296d4d5777fa9c14ebc8677a23577a87e
Picalines <picalines0@gmail.com>
2025-02-16 14:04:34 +0300

    refactor(synth): EventTimeline

---
+++ packages/synth/src/automation/readonly-event-timeline.ts
@@ -0,0 +1,20 @@
+import type { SynthTime } from '#time'
+
+export type TimedEvent = {
+  time: SynthTime
+}
+
+export type ReadonlyEventTimeline<TEvent extends TimedEvent> = {
+  get timeRange(): [start: SynthTime, end: SynthTime]
+
+  eventAt(time: SynthTime): TEvent | null
+  eventBefore(time: SynthTime): TEvent | null
+  eventBeforeOrAt(time: SynthTime): TEvent | null
+  eventAfter(time: SynthTime): TEvent | null
+  eventAfterOrAt(time: SynthTime): TEvent | null
+
+  eventsAfter(time: SynthTime): Iterable<TEvent>
+  eventsBefore(time: SynthTime): Iterable<TEvent>
+  eventsInRange(start: SynthTime, end: SynthTime): Iterable<TEvent>
+  eventSpan(time: SynthTime): [beforeOrAt: TEvent | null, after: TEvent | null]
+}
