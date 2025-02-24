b47d9665f9a5d545d98e35f2b2df4bb03c92bf70
Picalines <picalines0@gmail.com>
2025-02-24 16:57:06 +0300

    refactor(monorepo): headless vitest browser

--- packages/ui-kit/src/components/dialog/dialog.browser.spec.tsx
+++ packages/ui-kit/src/components/dialog/dialog.browser.spec.tsx
@@ -1,9 +1,9 @@
-import { expect, it } from 'vitest'
+import { expect, test } from 'vitest'
 import { render } from 'vitest-browser-react'
 import { Button } from '#components/button'
 import { Dialog } from '.'
 
-it('should open when the trigger is clicked', async () => {
+test('content should be visible after the trigger was clicked', async () => {
   const screen = render(
     <Dialog.Root>
       <Dialog.Trigger>
@@ -18,4 +18,21 @@ it('should open when the trigger is clicked', async () => {
   await screen.getByText('Trigger').click()
 
   await expect.element(screen.getByText('Content')).toBeVisible()
+})
+
+test('content should not be visible after close button was clicked', async () => {
+  const screen = render(
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button>Trigger</Button>
+      </Dialog.Trigger>
+      <Dialog.Title>Title</Dialog.Title>
+      <Dialog.Description>Description</Dialog.Description>
+      <Dialog.Content>Content</Dialog.Content>
+    </Dialog.Root>,
+  )
+
+  await screen.getByText('Trigger').click()
+  await screen.getByText('Close').click()
+  await expect.element(screen.getByText('Content')).not.toBeInTheDocument()
 })
