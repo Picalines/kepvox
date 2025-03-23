78ee03c641926869d3c94b3f6c263f5e55536a01
Picalines <picalines0@gmail.com>
2025-03-23 17:07:18 +0300

    infra: next-config package

---
+++ system/next-config/src/next-config.ts
@@ -0,0 +1,37 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  experimental: {
+    // TODO: might be optional
+    swcPlugins: [['@effector/swc-plugin', {}]],
+  },
+
+  webpack: (config, { dev }) => {
+    config.module.rules.push({
+      test: /^.+\.txt$/,
+      type: 'asset/source',
+    })
+
+    if (dev) {
+      config.plugins.push(new ImportConditionPlugin(['dev']))
+    }
+
+    return config
+  },
+}
+
+class ImportConditionPlugin {
+  readonly #conditions: string[]
+
+  constructor(conditions: string[]) {
+    this.#conditions = conditions
+  }
+
+  apply(compiler: any) {
+    compiler.hooks.afterEnvironment.tap(ImportConditionPlugin.name, () => {
+      compiler.options.resolve.conditionNames = [...compiler.options.resolve.conditionNames, ...this.#conditions]
+    })
+  }
+}
+
+export default nextConfig
