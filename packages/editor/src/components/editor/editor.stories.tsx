68bdab56156406f56e39d032839f2f07134e9bdf
Picalines <picalines0@gmail.com>
2025-03-23 15:33:53 +0300

    feat(editor): basic resizable panels

--- packages/editor/src/components/editor/editor.stories.tsx
+++ packages/editor/src/components/editor/editor.stories.tsx
@@ -4,6 +4,16 @@ import { Editor } from './editor'
 export default {
   title: 'components/Editor',
   component: Editor,
+  decorators: [
+    Story => (
+      <div className="h-dvh w-dvw">
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
 } satisfies Meta
 
 type Story = StoryObj
