45301050bb1326b633d61f7483df7c1058f08378
Picalines <picalines0@gmail.com>
2025-03-23 15:33:53 +0300

    feat(editor): basic resizable panels

--- packages/editor/src/components/editor/editor.tsx
+++ packages/editor/src/components/editor/editor.tsx
@@ -1,11 +1,25 @@
-import { Button } from '@repo/ui-kit/components/button'
+import { Resizable } from '@repo/ui-kit/components/resizable'
 import { Text } from '@repo/ui-kit/components/text'
 import type { FC } from 'react'
 
 export const Editor: FC = () => {
   return (
-    <Button>
-      <Text color="positive">@repo/editor configured!</Text>
-    </Button>
+    <Resizable.Group direction="horizontal" className="h-full w-full">
+      <Resizable.Panel>
+        <Text>timeline</Text>
+      </Resizable.Panel>
+      <Resizable.Handle />
+      <Resizable.Panel>
+        <Resizable.Group direction="vertical">
+          <Resizable.Panel>
+            <Text>nodes</Text>
+          </Resizable.Panel>
+          <Resizable.Handle />
+          <Resizable.Panel>
+            <Text>parameters</Text>
+          </Resizable.Panel>
+        </Resizable.Group>
+      </Resizable.Panel>
+    </Resizable.Group>
   )
 }
