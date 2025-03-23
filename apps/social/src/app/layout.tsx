5a3a79076f1d8478754dfc3b558e4532daa0acdd
Picalines <picalines0@gmail.com>
2025-03-23 17:32:36 +0300

    infra: blank social app

---
+++ apps/social/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { EffectorNext } from '@effector/next'
+import type { Metadata } from 'next'
+import { JetBrains_Mono } from 'next/font/google'
+import type { ReactNode } from 'react'
+
+import './index.css'
+
+export const metadata: Metadata = {
+  title: 'kepvox/social',
+}
+
+const monoFont = JetBrains_Mono({
+  subsets: ['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={monoFont.className}>
+        <EffectorNext>{children}</EffectorNext>
+      </body>
+    </html>
+  )
+}
---
+++ apps/social/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@repo/ui-kit/components/button'
+import { Text } from '@repo/ui-kit/components/text'
+
+export default function Home() {
+  return (
+    <Button>
+      <Text color="positive">social app configured!</Text>
+    </Button>
+  )
+}
