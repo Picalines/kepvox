d37c3e9af862644b0b90190f1818abea6219b0fb
Picalines <picalines0@gmail.com>
2025-03-23 15:33:30 +0300

    refactor(ui-kit): remove html props from resizable

--- packages/ui-kit/src/components/resizable/resizable.tsx
+++ packages/ui-kit/src/components/resizable/resizable.tsx
@@ -1,61 +1,43 @@
-import type { Overlay } from '@repo/common/typing'
-import type { FC, HTMLAttributes, ReactNode } from 'react'
+import type { FC, ReactNode } from 'react'
 import * as ResizablePrimitive from 'react-resizable-panels'
 import { cn } from '#lib/classnames'
 
-type HTMLElementTagName = keyof HTMLElementTagNameMap
+export type GroupProps = {
+  className?: string
+  children: ReactNode
+  direction: 'horizontal' | 'vertical'
+}
 
-type AnyHTMLElementAttributes = HTMLAttributes<HTMLElementTagNameMap[HTMLElementTagName]>
+export type PanelProps = {
+  className?: string
+  children: ReactNode
+  defaultSize?: number
+  maxSize?: number
+  minSize?: number
+  order?: number
+  onResize?: (size: number) => void
+}
 
-export type GroupProps = Overlay<
-  AnyHTMLElementAttributes,
-  {
-    children: ReactNode
-    direction: 'horizontal' | 'vertical'
-    as?: HTMLElementTagName
-  }
->
+export type HandleProps = {
+  className?: string
+  children?: ReactNode
+  disabled?: boolean
+  onFocus?: () => void
+  onBlur?: () => void
+}
 
-export type PanelProps = Overlay<
-  AnyHTMLElementAttributes,
-  {
-    children: ReactNode
-    as?: HTMLElementTagName
-    defaultSize?: number
-    maxSize?: number
-    minSize?: number
-    order?: number
-    onResize?: (size: number) => void
-  }
->
-
-export type HandleProps = Overlay<
-  AnyHTMLElementAttributes,
-  {
-    children: ReactNode
-    as?: HTMLElementTagName
-    disabled?: boolean
-    onFocus?: () => void
-    onBlur?: () => void
-  }
->
-
-export const Group: FC<GroupProps> = ({ className, as, ...props }) => (
-  // @ts-expect-error react-resizable-panels specifies its props as HTMLAttributes<'div' | etc>, where HTMLAttributes actually expects HTMLDivElement | etc
+export const Group: FC<GroupProps> = ({ className, ...props }) => (
   <ResizablePrimitive.PanelGroup
     {...props}
-    tagName={as}
     className={cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', className)}
   />
 )
 
-export const Panel: FC<PanelProps> = ({ as, ...props }) => <ResizablePrimitive.Panel {...props} tagName={as} />
+export const Panel: FC<PanelProps> = props => <ResizablePrimitive.Panel {...props} />
 
-export const Handle: FC<HandleProps> = ({ className, as, children, ...props }) => (
-  // @ts-expect-error react-resizable-panels specifies its props as HTMLAttributes<'div' | etc>, where HTMLAttributes actually expects HTMLDivElement | etc
+export const Handle: FC<HandleProps> = ({ className, children, ...props }) => (
   <ResizablePrimitive.PanelResizeHandle
     {...props}
-    tagName={as}
     className={cn(
       'after:-translate-x-1/2 data-[panel-group-direction=vertical]:after:-translate-y-1/2 relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
       className,
