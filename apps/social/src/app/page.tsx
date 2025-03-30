303779378a92fe214f9c4659c6205fbe7db32f30
Picalines <picalines0@gmail.com>
2025-03-30 23:23:35 +0300

    feat(social): very basic auth pages
    to prove that `next build` works

--- apps/social/src/app/page.tsx
+++ apps/social/src/app/page.tsx
@@ -1,19 +1,19 @@
 'use server'
 
-import { Text } from '@repo/ui-kit/components/text'
+import { buttonVariants } from '@repo/ui-kit/components/button'
+import Link from 'next/link'
 import type { FC } from 'react'
-import { DraftAuth } from './draft-auth'
 
 const HomePage: FC = async () => {
   return (
-    <>
-      <div>
-        <Text variant="heading-m">@kevpox/social</Text>
-      </div>
-      <div>
-        <DraftAuth />
-      </div>
-    </>
+    <div className="flex gap-2">
+      <Link href="/sign-in" prefetch={false} className={buttonVariants()}>
+        Sign in
+      </Link>
+      <Link href="/account" prefetch={false} className={buttonVariants()}>
+        Account
+      </Link>
+    </div>
   )
 }
 
