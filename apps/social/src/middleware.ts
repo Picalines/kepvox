eaca9645ca0cb023b54b0cc3a8efc07d6abf3806
Picalines <picalines0@gmail.com>
2025-03-30 22:47:19 +0300

    feat(social): default auth middleware

---
+++ apps/social/src/middleware.ts
@@ -0,0 +1,23 @@
+import { createAuthClient } from 'better-auth/client'
+import { headers } from 'next/headers'
+import { type NextRequest, NextResponse } from 'next/server'
+
+const authClient = createAuthClient()
+
+export async function middleware(request: NextRequest) {
+  const { data: session } = await authClient.getSession({
+    fetchOptions: {
+      headers: await headers(),
+    },
+  })
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/account', '/library', '/projects', '/projects/:path*'],
+}
