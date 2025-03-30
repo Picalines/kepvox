71a5fdd73d917cce175fba95ea8d0826825b9de3
Picalines <picalines0@gmail.com>
2025-03-30 23:23:35 +0300

    feat(social): very basic auth pages
    to prove that `next build` works

--- apps/social/src/app/(id)/account/page.tsx
+++ apps/social/src/app/(id)/account/page.tsx
@@ -1,7 +1,19 @@
-import type { FC } from 'react'
+'use server'
 
-const AccountPage: FC = () => {
-  return null
+import { Text } from '@repo/ui-kit/components/text'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import type { FC } from 'react'
+import { authServer } from '#shared/auth-server'
+
+const AccountPage: FC = async () => {
+  const session = await authServer.api.getSession({ headers: await headers() })
+
+  if (!session) {
+    redirect('/sign-in')
+  }
+
+  return <Text>Current user: {session.user.name}</Text>
 }
 
 export default AccountPage
--- apps/social/src/app/(id)/sign-in/page.tsx
+++ apps/social/src/app/(id)/sign-in/page.tsx
@@ -1,7 +1,30 @@
-import type { FC } from 'react'
+'use client'
+
+import { Button } from '@repo/ui-kit/components/button'
+import { Loader } from '@repo/ui-kit/components/loader'
+import { useRouter } from 'next/navigation'
+import { type FC, useCallback, useEffect } from 'react'
+import { authClient } from '#shared/auth-client'
 
 const SignInPage: FC = () => {
-  return null
+  const router = useRouter()
+  const { isPending, data } = authClient.useSession()
+
+  const onLogin = useCallback(async () => {
+    await authClient.signIn.social({ provider: 'github' })
+  }, [])
+
+  useEffect(() => {
+    if (data) {
+      router.push('/account')
+    }
+  }, [data, router])
+
+  if (data || isPending) {
+    return <Loader />
+  }
+
+  return <Button onClick={onLogin}>Login with GitHub</Button>
 }
 
 export default SignInPage
