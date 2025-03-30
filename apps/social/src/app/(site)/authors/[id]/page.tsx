d2345e5502e5835a1f90af565a8cf76a5cf2b32c
Picalines <picalines0@gmail.com>
2025-03-30 22:01:50 +0300

    feat(social): blank page components
    Needed for next.js build

---
+++ apps/social/src/app/(site)/authors/[id]/page.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const AuthorPage: FC = () => {
+  return null
+}
+
+export default AuthorPage
---
+++ apps/social/src/app/(user)/projects/[id]/publish/page.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react'
+
+const PublishPage: FC = () => {
+  return null
+}
+
+export default PublishPage
