b9812f6e8a0dd7d3572fbfbe1a331a2a3801e885
Picalines <picalines0@gmail.com>
2025-04-01 10:17:21 +0300

    feat(social): authenticateOrRedirect

--- apps/social/src/shared/auth-server/auth-server.ts
+++ apps/social/src/shared/auth-server/auth-server.ts
@@ -3,6 +3,9 @@ import 'server-only'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
+import type { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers'
+import { headers } from 'next/headers'
+import { RedirectType, redirect } from 'next/navigation'
 import { database, tables } from '#shared/database'
 import { ENV } from '#shared/env'
 
@@ -19,3 +22,22 @@ export const authServer = betterAuth({
     },
   },
 })
+
+type AuthenticateOrRedirectParams = {
+  headers?: ReadonlyHeaders
+  fallbackUrl?: string
+}
+
+export const authenticateOrRedirect = async (params?: AuthenticateOrRedirectParams) => {
+  const { headers: headersParam, fallbackUrl = '/sign-in' } = params ?? {}
+
+  const session = await authServer.api.getSession({
+    headers: headersParam ?? (await headers()),
+  })
+
+  if (!session) {
+    redirect(fallbackUrl, RedirectType.replace)
+  }
+
+  return session
+}
