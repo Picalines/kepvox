53f2414bd83fc55dd29ae1a0fe85179f2e6abef0
Picalines <picalines0@gmail.com>
2025-04-01 22:06:15 +0300

    feat(social): updateProject name & description

--- apps/social/src/app/(user)/projects/[id]/update-project.ts
+++ apps/social/src/app/(user)/projects/[id]/update-project.ts
@@ -11,24 +11,29 @@ import { projectSchema } from '#shared/schema'
 const inputSchema = z.object({
   project: z.object({
     id: z.string().min(1),
-    content: projectSchema,
+    name: z.string().min(1).max(75).optional(),
+    description: z.string().min(0).max(300).optional(),
+    content: projectSchema.optional(),
   }),
 })
 
 export const updateProject = async (input: z.infer<typeof inputSchema>) => {
-  if (!inputSchema.safeParse(input).success) {
-    throw new Error('invalid project content')
-  }
+  inputSchema.parse(input)
 
   const {
-    project: { id: projectId, content },
+    project: { id: projectId, name, description, content },
   } = input
 
   const { user } = await authenticateOrRedirect()
 
   const updatedProjects = await database
     .update(tables.project)
-    .set({ content, updatedAt: new Date() })
+    .set({
+      content,
+      name,
+      description,
+      updatedAt: new Date(),
+    })
     .where(and(eq(tables.project.authorId, user.id), eq(tables.project.id, projectId)))
     .returning({ id: tables.project.id })
 
