13520f28e86ec13ae0817f79e49edcf57d89e9f9
Picalines <picalines0@gmail.com>
2025-04-03 18:54:26 +0300

    feat(social): searchAuthors action

--- apps/social/src/app/(site)/authors/page.tsx
+++ apps/social/src/app/(site)/authors/page.tsx
@@ -1,7 +1,25 @@
-import type { FC } from 'react'
+'use server'
 
-const AuthorsPage: FC = () => {
-  return null
+import { Text } from '@repo/ui-kit/components/text'
+import type { FC } from 'react'
+import { searchAuthors } from './search-authors'
+
+type Props = {
+  searchParams?: Promise<{ name?: string }>
+}
+
+const AuthorsPage: FC<Props> = async props => {
+  const { searchParams } = props
+
+  const { name = '' } = (await searchParams) ?? {}
+
+  const authorsData = await searchAuthors({ namePart: name })
+
+  return (
+    <Text>
+      <pre>{JSON.stringify(authorsData, null, 2)}</pre>
+    </Text>
+  )
 }
 
 export default AuthorsPage
