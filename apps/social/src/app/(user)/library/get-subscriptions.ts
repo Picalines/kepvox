464735678e8622c72b97f8d29cd05e1c8cfd8176
Picalines <picalines0@gmail.com>
2025-04-03 20:06:41 +0300

    feat(social): getSubscriptionOption

---
+++ apps/social/src/app/(user)/library/get-subscriptions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { and, desc, eq } from 'drizzle-orm'
+import { authenticateOrRedirect } from '#shared/auth-server'
+import { database, tables } from '#shared/database'
+
+export const getSubscriptions = async () => {
+  const { user } = await authenticateOrRedirect()
+
+  const subscriptions = await database
+    .select({ id: tables.user.id, name: tables.user.name })
+    .from(tables.user)
+    .innerJoin(
+      tables.subscription,
+      and(eq(tables.subscription.listenerId, user.id), eq(tables.subscription.authorId, tables.user.id)),
+    )
+    .orderBy(desc(tables.subscription.createdAt))
+
+  return { subscriptions }
+}
