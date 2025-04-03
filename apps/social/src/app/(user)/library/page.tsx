818831b3e49558b70d933ebb75b83089a2df123a
Picalines <picalines0@gmail.com>
2025-04-03 20:06:41 +0300

    feat(social): getSubscriptionOption

--- apps/social/src/app/(user)/library/page.tsx
+++ apps/social/src/app/(user)/library/page.tsx
@@ -3,14 +3,26 @@
 import { Text } from '@repo/ui-kit/components/text'
 import type { FC } from 'react'
 import { getReactions } from './get-reactions'
+import { getSubscriptions } from './get-subscriptions'
 
 const LibraryPage: FC = async () => {
   const reactionsData = await getReactions()
 
+  const subscriptions = await getSubscriptions()
+
   return (
-    <Text>
-      <pre>{JSON.stringify(reactionsData, null, 2)}</pre>
-    </Text>
+    <>
+      <div>
+        <Text>
+          <pre>{JSON.stringify(reactionsData, null, 2)}</pre>
+        </Text>
+      </div>
+      <div>
+        <Text>
+          <pre>{JSON.stringify(subscriptions, null, 2)}</pre>
+        </Text>
+      </div>
+    </>
   )
 }
 
