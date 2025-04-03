d277ea71990c1d540330a6680496e9fb85cd0869
Picalines <picalines0@gmail.com>
2025-04-04 00:12:51 +0300

    feat(ui-kit): standard resizable colors

--- packages/ui-kit/src/components/resizable/resizable.tsx
+++ packages/ui-kit/src/components/resizable/resizable.tsx
@@ -39,12 +39,12 @@ export const Handle: FC<HandleProps> = ({ className, children, ...props }) => (
   <ResizablePrimitive.PanelResizeHandle
     {...props}
     className={cn(
-      'after:-translate-x-1/2 data-[panel-group-direction=vertical]:after:-translate-y-1/2 group relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:bg-blue-500 after:transition-all focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=horizontal]:data-[resize-handle-state=inactive]:after:w-0 data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[resize-handle-state=inactive]:after:h-0 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[resize-handle-state=inactive]:after:bg-transparent [&[data-panel-group-direction=vertical]>div]:rotate-90',
+      'after:-translate-x-1/2 data-[panel-group-direction=vertical]:after:-translate-y-1/2 group relative flex items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:bg-primary after:transition-all focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=horizontal]:w-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:data-[resize-handle-state=inactive]:after:h-0 data-[panel-group-direction=horizontal]:data-[resize-handle-state=inactive]:after:w-0 data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=horizontal]:after:w-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[resize-handle-state=inactive]:after:bg-transparent [&[data-panel-group-direction=vertical]>div]:rotate-90',
       className,
     )}
   >
     {children && (
-      <div className="z-1 rounded-sm bg-blue-500 p-1/2 transition-all group-data-[resize-handle-state=inactive]:bg-border">
+      <div className="z-1 rounded-sm bg-border p-1/2 ring-4 ring-primary transition-all group-data-[resize-handle-state=inactive]:ring-0">
         {children}
       </div>
     )}
