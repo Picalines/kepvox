2ee49f0c4b80a020e426db70351c6c86a5ac916a
Picalines <picalines0@gmail.com>
2025-04-04 19:53:21 +0300

    feat(social): site sidebar

---
+++ apps/social/src/widgets/site-sidebar/ui/account-sidebar-link.tsx
@@ -0,0 +1,31 @@
+'use server'
+
+import { DoorOpenIcon, UserIcon } from '@repo/ui-kit/icons'
+import type { FC } from 'react'
+import { authenticateOrNull } from '#shared/auth-server'
+import { SidebarLink } from './sidebar-link'
+
+export const AccountSidebarLink: FC = async () => {
+  const session = await authenticateOrNull()
+
+  if (!session) {
+    return (
+      <SidebarLink href="/sign-in" icon={<DoorOpenIcon />}>
+        Sign In
+      </SidebarLink>
+    )
+  }
+
+  const {
+    user: { name: username, image: avatar },
+  } = session
+
+  return (
+    <SidebarLink
+      href="/account"
+      icon={avatar ? <img src={avatar} alt={`${username}'s avatar`} className="rounded-full" /> : <UserIcon />}
+    >
+      Account
+    </SidebarLink>
+  )
+}
