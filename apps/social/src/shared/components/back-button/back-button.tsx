c00b6b79dd1e7574a1a9962d1b287611b428d122
Picalines <picalines0@gmail.com>
2025-04-05 16:07:47 +0300

    feat(social): <BackButton> fallback

--- apps/social/src/shared/components/back-button/back-button.tsx
+++ apps/social/src/shared/components/back-button/back-button.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import type { OmitExisting } from '@repo/common/typing'
+import type { OmitExisting, Overlay } from '@repo/common/typing'
 import { Button } from '@repo/ui-kit/components/button'
 import { ArrowLeftIcon } from '@repo/ui-kit/icons'
 import { useRouter } from 'next/navigation'
 import type { ComponentProps, FC } from 'react'
 
-type Props = OmitExisting<ComponentProps<typeof Button>, 'onClick'>
+type Props = Overlay<OmitExisting<ComponentProps<typeof Button>, 'onClick'>, { fallbackPath: string }>
 
 export const BackButton: FC<Props> = props => {
-  const { back } = useRouter()
+  const { fallbackPath, children, ...buttonProps } = props
+
+  const { back, replace } = useRouter()
+
+  const onClick = () => {
+    if (window.history.length > 1) {
+      back()
+    } else {
+      replace(fallbackPath)
+    }
+  }
 
   return (
-    <Button {...props} onClick={back}>
-      <ArrowLeftIcon />
+    <Button {...buttonProps} onClick={onClick}>
+      {children ? children : <ArrowLeftIcon />}
     </Button>
   )
 }
