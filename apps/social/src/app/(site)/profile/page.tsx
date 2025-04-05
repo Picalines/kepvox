6af898f8b985fdffa474bec013142f5f2ac15e91
Picalines <picalines0@gmail.com>
2025-04-05 20:07:52 +0300

    feat(social): basic profile screen

--- apps/social/src/app/(site)/profile/page.tsx
+++ apps/social/src/app/(site)/profile/page.tsx
@@ -1,27 +1,8 @@
-'use server'
-
-import { buttonVariants } from '@repo/ui-kit/components/button'
-import { Text } from '@repo/ui-kit/components/text'
-import Link from 'next/link'
 import type { FC } from 'react'
-import { authenticateOrRedirect } from '#shared/auth-server'
+import { ProfileScreen } from '#screens/profile'
 
-const AccountPage: FC = async () => {
-  const { user } = await authenticateOrRedirect()
-
-  return (
-    <>
-      <div>
-        <Text>Current user: {user.name}</Text>
-      </div>
-      <Link href="/projects" prefetch={false} className={buttonVariants()}>
-        Projects
-      </Link>
-      <Link href="/sign-out" prefetch={false} className={buttonVariants()}>
-        Sign out
-      </Link>
-    </>
-  )
+const ProfilePage: FC = () => {
+  return <ProfileScreen />
 }
 
-export default AccountPage
+export default ProfilePage
---
+++ apps/social/src/screens/profile/ui/profile-screen.tsx
@@ -0,0 +1,34 @@
+'use server'
+
+import { buttonVariants } from '@repo/ui-kit/components/button'
+import { Heading } from '@repo/ui-kit/components/heading'
+import Link from 'next/link'
+import type { FC } from 'react'
+import { authenticateOrRedirect } from '#shared/auth-server'
+import { Avatar } from '#shared/components/avatar'
+import { RelativeDate } from '#shared/components/relative-date'
+
+export const ProfileScreen: FC = async () => {
+  const { user } = await authenticateOrRedirect()
+
+  return (
+    <div className="space-y-2">
+      <div className="flex items-center gap-2">
+        <Avatar.Root className="size-20">
+          <Avatar.Image src={user.image} />
+          <Avatar.Fallback userName={user.name} />
+        </Avatar.Root>
+        <Heading.Root>
+          <Heading.Title>{user.name}</Heading.Title>
+          <Heading.Description>
+            Joined <RelativeDate date={user.createdAt} suffix />
+          </Heading.Description>
+        </Heading.Root>
+      </div>
+
+      <Link href="/sign-out" prefetch={false} className={buttonVariants({ variant: 'outline' })}>
+        Sign out
+      </Link>
+    </div>
+  )
+}
