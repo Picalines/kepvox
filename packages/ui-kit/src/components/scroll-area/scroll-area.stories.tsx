5181deb50d7f8aa2b7192bd419710239d47c7915
Picalines <picalines0@gmail.com>
2025-04-06 20:59:14 +0300

    feat(ui-kit): <ScrollArea/>

---
+++ packages/ui-kit/src/components/scroll-area/scroll-area.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ScrollArea } from '.'
+
+type StoryArgs = ScrollArea.RootProps & {
+  orientation: ScrollArea.BarProps['orientation'] | 'both'
+}
+
+export default {
+  title: 'components/ScrollArea',
+  component: ScrollArea.Root,
+  decorators: [
+    Story => (
+      <div className="size-50">
+        <Story />
+      </div>
+    ),
+  ],
+  render: ({ orientation }) => (
+    <ScrollArea.Root>
+      {orientation === 'both' && <ScrollArea.Bar orientation="vertical" />}
+      {orientation === 'both' && <ScrollArea.Bar orientation="horizontal" />}
+      {orientation !== 'both' && <ScrollArea.Bar orientation={orientation} />}
+      <ScrollArea.Content>
+        <div className="grid size-128 grid-cols-8 gap-1">
+          {Array.from({ length: 64 }).map((_, i) => (
+            <div key={String(i)} className="border text-center">
+              {i}
+            </div>
+          ))}
+        </div>
+      </ScrollArea.Content>
+    </ScrollArea.Root>
+  ),
+} satisfies Meta<StoryArgs>
+
+type Story = StoryObj<StoryArgs>
+
+export const Vertical: Story = {
+  args: {
+    orientation: 'vertical',
+  },
+}
+
+export const Horizontal: Story = {
+  args: {
+    orientation: 'horizontal',
+  },
+}
+
+export const Both: Story = {
+  args: {
+    orientation: 'both',
+  },
+}
