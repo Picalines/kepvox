a944c0a97de539630baf2b5d624c2843e5f5dac6
Picalines <picalines0@gmail.com>
2025-04-08 00:50:22 +0300

    feat(social): landing transitions to make it static

--- apps/social/src/screens/landing/ui/account-link.tsx
+++ apps/social/src/screens/landing/ui/account-link.tsx
@@ -1,17 +1,21 @@
-'use server'
+'use client'
 
 import { Button } from '@repo/ui-kit/components/button'
 import Link from 'next/link'
 import type { FC } from 'react'
-import { authenticateOrNull } from '#shared/auth-server'
+import { authClient } from '#shared/auth-client'
 import { Avatar } from '#shared/components/avatar'
 
-export const AccountLink: FC = async () => {
-  const session = await authenticateOrNull()
+export const AccountLink: FC = () => {
+  const { data: session, isPending } = authClient.useSession()
+
+  if (isPending) {
+    return null
+  }
 
   if (!session) {
     return (
-      <Button asChild variant="ghost">
+      <Button asChild variant="ghost" className="fade-in-0 animate-in">
         <Link href="/sign-in" prefetch={false}>
           Sign in
         </Link>
@@ -24,7 +28,7 @@ export const AccountLink: FC = async () => {
   } = session
 
   return (
-    <Button asChild variant="ghost" className="p-2">
+    <Button asChild variant="ghost" className="fade-in-0 animate-in p-2">
       <Link href="/profile" prefetch={false}>
         <Avatar.Root className="size-6">
           <Avatar.Image src={avatar} />
--- apps/social/src/screens/landing/ui/site-entrypoint.tsx
+++ apps/social/src/screens/landing/ui/site-entrypoint.tsx
@@ -2,15 +2,15 @@ import { Button } from '@repo/ui-kit/components/button'
 import Link from 'next/link'
 import type { FC } from 'react'
 
-export const SiteEntrypoint: FC = async () => {
+export const SiteEntrypoint: FC = () => {
   return (
     <div className="flex gap-2">
-      <Button asChild variant="outline">
+      <Button asChild variant="ghost">
         <Link href="/tracks" prefetch={false}>
           Explore
         </Link>
       </Button>
-      <Button asChild variant="outline">
+      <Button asChild variant="ghost">
         <Link href="/projects" prefetch={false}>
           Create
         </Link>
