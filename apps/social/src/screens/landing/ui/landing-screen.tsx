9505c5e84fdd595471ff1ca39776c61d934eee01
Picalines <picalines0@gmail.com>
2025-04-08 00:50:22 +0300

    feat(social): landing transitions to make it static

--- apps/social/src/screens/landing/ui/landing-screen.tsx
+++ apps/social/src/screens/landing/ui/landing-screen.tsx
@@ -1,5 +1,4 @@
-import { Skeleton } from '@repo/ui-kit/components/skeleton'
-import { type FC, Suspense } from 'react'
+import type { FC } from 'react'
 import { ThemeSwitcher } from '#shared/components/theme-switcher'
 import { AccountLink } from './account-link'
 import { LandingLogo } from './landing-logo'
@@ -8,17 +7,15 @@ import { SiteEntrypoint } from './site-entrypoint'
 export const LandingScreen: FC = () => {
   return (
     <main className="w-full">
-      <div className="sticky top-0 flex border-b p-2">
-        <Suspense fallback={<Skeleton />}>
-          <AccountLink />
-        </Suspense>
+      <div className="fade-in-0 sticky top-0 flex animate-in border-b p-2 duration-200">
+        <AccountLink />
         <div className="inline flex-grow" />
         <ThemeSwitcher variant="ghost" />
       </div>
       <div className="relative h-[60vh]">
         <LandingLogo className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2" />
       </div>
-      <div className="mx-auto w-min">
+      <div className="fade-in-0 slide-in-from-bottom-2 mx-auto w-min animate-in fill-mode-both transition-transform delay-400 duration-400">
         <SiteEntrypoint />
       </div>
     </main>
