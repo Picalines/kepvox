4822689ef393b7aa044c5956addb50ac156f6577
Picalines <picalines0@gmail.com>
2025-04-11 23:13:57 +0300

    fix(editor): add ranges for missing units

--- packages/editor/src/meta/editor-node-properties.ts
+++ packages/editor/src/meta/editor-node-properties.ts
@@ -5,6 +5,10 @@ import { Pitch, type UnitName } from '@repo/synth'
  * here to show more sensible range in the editor
  */
 export const USER_UNIT_RANGES: Partial<Record<UnitName, { min: number; max: number }>> = {
+  seconds: { min: 0, max: 30 },
   notes: { min: 0, max: 64 },
+  decibels: { min: -80, max: 5 },
   hertz: { min: 0, max: Pitch.frequency('B9') },
+  nonNegative: { min: 0, max: 100 },
+  factor: { min: -5, max: 5 },
 }
