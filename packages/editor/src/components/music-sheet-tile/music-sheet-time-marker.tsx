43fb40649f69d9b793c51c25b106b599d88e5102
Picalines <picalines0@gmail.com>
2025-04-20 00:43:31 +0300

    fix(editor): use primary & destructive colors for markers

--- packages/editor/src/components/music-sheet-tile/music-sheet-time-marker.tsx
+++ packages/editor/src/components/music-sheet-tile/music-sheet-time-marker.tsx
@@ -1,36 +1,28 @@
 import { SynthTime } from '@repo/synth'
-import { cn, tw } from '@repo/ui-kit/classnames'
+import { cn } from '@repo/ui-kit/classnames'
 import { useUnit } from 'effector-react'
 import type { FC } from 'react'
 import { editorModel } from '#model'
 import type { MusicSheetDimensions } from './music-sheet-dimensions'
 
-type MarkerColor = 'green' | 'red' | 'muted'
-
 type Props = {
   dimensions: MusicSheetDimensions
-  color: MarkerColor
   time: SynthTime
+  className?: string
 }
 
 export const MusicSheetTimeMarker: FC<Props> = props => {
-  const { dimensions, color, time } = props
+  const { dimensions, time, className } = props
 
   const { position } = useUnit({ position: editorModel.$sheetPosition })
 
   return (
     <div
       className={cn(
-        '-translate-x-1/2 pointer-events-none absolute top-0 bottom-0 z-10 w-0.5 opacity-75 transition-colors',
-        MARKER_COLOR_CLASSNAMES[color],
+        '-translate-x-1/2 pointer-events-none absolute top-0 bottom-0 z-10 w-0.5 bg-accent transition-colors',
+        className,
       )}
       style={{ left: position.x + time.toNotes() * dimensions.note.width(SynthTime.note) }}
     />
   )
-}
-
-const MARKER_COLOR_CLASSNAMES: Record<MarkerColor, string> = {
-  green: tw`bg-emerald-500`,
-  red: tw`bg-rose-500`,
-  muted: tw`bg-muted`,
 }
