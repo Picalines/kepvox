f6b417de8087fee30722fb1002411a8db8da1e98
Picalines <picalines0@gmail.com>
2025-04-19 23:46:14 +0300

    fix(ui-kit): slider label like for inputs

--- packages/ui-kit/src/components/slider/slider.tsx
+++ packages/ui-kit/src/components/slider/slider.tsx
@@ -52,27 +52,26 @@ export const Root: FC<RootProps> = props => {
         value={value !== undefined ? [value] : undefined}
         defaultValue={defaultValue !== undefined ? [defaultValue] : undefined}
         onValueChange={onValueChange}
-        className="group relative flex h-10 w-full cursor-ew-resize touch-none select-none items-center overflow-clip rounded-md border border-border ring-offset-background transition-all focus-within:ring-2 focus-within:ring-offset-2 data-disabled:cursor-not-allowed"
+        className="group peer relative flex h-10 w-full cursor-ew-resize touch-none select-none items-center overflow-clip rounded-md border border-border ring-offset-background transition-all focus-within:ring-2 focus-within:ring-offset-2 data-disabled:cursor-not-allowed"
       >
-        <RadixSlider.Track className="relative h-full grow border-border bg-secondary group-hover:bg-secondary/90">
-          <RadixSlider.Range className="absolute h-full bg-primary transition-colors group-hover:bg-primary/90 group-data-disabled:opacity-75" />
+        <RadixSlider.Track className="relative h-full grow bg-background">
+          <RadixSlider.Range className="absolute h-full bg-accent transition-colors group-hover:bg-accent/90 group-data-disabled:opacity-75" />
         </RadixSlider.Track>
         <RadixSlider.Thumb aria-label={label?.children ? String(label.children) : undefined} />
       </RadixSlider.Root>
+      {label?.children && (
+        <span className="-translate-y-1/2 peer-focus-within:-top-1 pointer-events-none absolute top-0 left-3 origin-left translate-x-[-2px] rounded-xs border-[2px] border-background bg-background text-muted-foreground text-xs leading-none transition-all peer-focus-within:text-ring peer-data-disabled:text-muted-foreground/50">
+          {label.children}
+        </span>
+      )}
       <span
         aria-hidden
         className={cn(
-          '-translate-y-1/2 pointer-events-none absolute top-1/2 left-3 text-white mix-blend-difference',
-          label?.children && 'text-xs leading-none',
+          '-translate-y-1/2 pointer-events-none absolute top-1/2 left-3 text-accent-foreground text-sm peer-data-disabled:text-accent-foreground/50',
         )}
+        ref={sliderValueRef}
       >
-        {label?.children && (
-          <>
-            {label.children}
-            <br />
-          </>
-        )}
-        <span ref={sliderValueRef}>{sliderValueRef.current?.innerText ?? defaultValue ?? value}</span>
+        {sliderValueRef.current?.innerText ?? defaultValue ?? value}
       </span>
     </div>
   )
