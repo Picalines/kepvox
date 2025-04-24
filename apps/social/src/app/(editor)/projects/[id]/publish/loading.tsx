78bd68e3c99a55f7188d27efe0286b150df301a2
Picalines <picalines0@gmail.com>
2025-04-25 00:10:11 +0300

    fix(social): publish page loader

---
+++ apps/social/src/app/(editor)/projects/[id]/publish/loading.tsx
@@ -0,0 +1,8 @@
+import { Loader } from '@repo/ui-kit/components/loader'
+import type { FC } from 'react'
+
+const PublishPageLoader: FC = () => {
+  return <Loader centered />
+}
+
+export default PublishPageLoader
