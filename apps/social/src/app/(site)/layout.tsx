27be38bb92959817427538282a713ff7f81090be
Picalines <picalines0@gmail.com>
2025-04-25 00:05:55 +0300

    refactor(social): tooltip provider in root layout

--- apps/social/src/app/(site)/layout.tsx
+++ apps/social/src/app/(site)/layout.tsx
@@ -1,4 +1,3 @@
-import { Tooltip } from '@repo/ui-kit/components/tooltip'
 import type { FC, ReactNode } from 'react'
 import { SiteSidebar } from '#widgets/site-sidebar'
 
@@ -10,12 +9,10 @@ const SiteLayout: FC<Props> = async props => {
   const { children } = props
 
   return (
-    <Tooltip.Provider>
-      <div className="flex h-dvh w-dvw">
-        <SiteSidebar />
-        <main className="grow overflow-auto p-4">{children}</main>
-      </div>
-    </Tooltip.Provider>
+    <div className="flex h-dvh w-dvw">
+      <SiteSidebar />
+      <main className="grow overflow-auto p-4">{children}</main>
+    </div>
   )
 }
 
