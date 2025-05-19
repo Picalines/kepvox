dfb85d02b2172ff22a8065ba61cee71b9cbfc61e
Picalines <picalines0@gmail.com>
2025-05-19 20:31:21 +0300

    feat(social): /author/:id screen

--- apps/social/src/app/(site)/authors/[id]/page.tsx
+++ apps/social/src/app/(site)/authors/[id]/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from '@repo/ui-kit/components/button'
-import { Text } from '@repo/ui-kit/components/text'
 import type { FC } from 'react'
-import { getAuthor } from './get-author'
-import { subscribeToAuthor } from './subscribe-to-author'
-import { unsubscribeFromAuthor } from './unsubscribe-from-author'
+import { AuthorScreen } from '#screens/author'
 
 type Props = {
   params: Promise<{ id: string }>
@@ -12,35 +8,9 @@ type Props = {
 const AuthorPage: FC<Props> = async props => {
   const { params } = props
 
-  const { id: authorId } = await params
+  const { id } = await params
 
-  const authorData = await getAuthor({ author: { id: authorId } })
-
-  return (
-    <>
-      <form
-        action={async () => {
-          'use server'
-          await subscribeToAuthor({ author: { id: authorId } })
-        }}
-      >
-        <Button type="submit">Subscribe</Button>
-      </form>
-      <form
-        action={async () => {
-          'use server'
-          await unsubscribeFromAuthor({ author: { id: authorId } })
-        }}
-      >
-        <Button type="submit">Unsubscribe</Button>
-      </form>
-      <div>
-        <Text>
-          <pre>{JSON.stringify(authorData, null, 2)}</pre>
-        </Text>
-      </div>
-    </>
-  )
+  return <AuthorScreen id={id} />
 }
 
 export default AuthorPage
