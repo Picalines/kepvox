4a2bc56cbabd86588798712083456a6e8212e6fd
Picalines <picalines0@gmail.com>
2025-07-12 21:18:57 +0300

    chore(ui-kit): remove Skeleton

--- packages/ui-kit/src/components/skeleton/skeleton.screen.spec.ts
+++
@@ -1,9 +0,0 @@
-import { testStoryMatrix } from '@repo/test-screen/test-story'
-
-testStoryMatrix({
-  meta: {
-    title: 'components/Skeleton',
-  },
-  stories: ['Default'],
-  themes: ['light', 'dark'],
-})
