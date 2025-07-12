870732ccb8c88e23a526355bf76db2e6f854faa3
Picalines <picalines0@gmail.com>
2025-07-12 20:05:15 +0300

    chore(ui-kit): remove loader classnames

--- packages/ui-kit/src/components/loader/loader.tsx
+++ packages/ui-kit/src/components/loader/loader.tsx
@@ -1,17 +1,17 @@
 import type { FC } from 'react'
 import { LoaderIcon } from '#icons'
-import { cn } from '#lib/classnames'
 
 export type LoaderProps = {
-  className?: string
   centered?: boolean
 }
 
-export const Loader: FC<LoaderProps> = ({ className, centered = false }) => {
+export const Loader: FC<LoaderProps> = props => {
+  const { centered = false } = props
+
   const icon = <LoaderIcon className="animate-spin" />
 
   return (
-    <div className={cn('w-min', className)}>
+    <div className="w-min">
       {centered ? <div className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2">{icon}</div> : icon}
     </div>
   )
