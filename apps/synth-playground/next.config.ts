83ba6ed9cc55e7e654f0630a6619329efc5b7d4e
Picalines <picalines0@gmail.com>
2025-07-19 17:10:45 +0300

    feat(synth-playground): static build for pages

--- apps/synth-playground/next.config.ts
+++ apps/synth-playground/next.config.ts
@@ -1,1 +1,15 @@
-export { default } from '@repo/next-config'
+import baseConfig from '@repo/next-config'
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  ...baseConfig,
+
+  output: 'export',
+  basePath: '/kepvox/synth-playground',
+
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
