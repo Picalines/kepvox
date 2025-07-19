5f31acebb33687ad969233573dba5734d93e4695
Picalines <picalines0@gmail.com>
2025-07-19 21:47:09 +0300

    fix(ui-kit): remove unused with-classname

--- packages/ui-kit/src/lib/hocs/with-classname.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react'
-import { cn } from '#lib/classnames'
-
-export function withClassName<P extends { className?: string } = {}>(
-  Component: FC<P>,
-  ...cnParams: Parameters<typeof cn>
-): FC<P> {
-  return props => <Component {...props} className={cn(...cnParams, props.className)} />
-}
