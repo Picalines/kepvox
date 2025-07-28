3ba53ef3d893109c45a19636a02d20ce082ea133
Picalines <picalines0@gmail.com>
2025-07-28 23:18:01 +0300

    feat(synth-playground): initial code with imports

--- apps/synth-playground/src/screens/editor-screen/model/code-editor.ts
+++ apps/synth-playground/src/screens/editor-screen/model/code-editor.ts
@@ -1,9 +1,10 @@
 import { createFactory } from '@withease/factories'
 import { createEvent, createStore, sample } from 'effector'
 import { not, readonly } from 'patronum'
+import INITIAL_CODE from '#public/initial-code.txt'
 
 export const createCodeEditor = createFactory(() => {
-  const $code = createStore('')
+  const $code = createStore(INITIAL_CODE)
 
   const $isReadonly = createStore(false)
 
