a6b7af906f0ed265cc9f1a6d4b87bc20b987e3f4
Picalines <picalines0@gmail.com>
2025-07-29 23:39:44 +0300

    feat(synth-playground): heading on right

--- apps/synth-playground/src/screens/editor-screen/ui/editor-header.tsx
+++ apps/synth-playground/src/screens/editor-screen/ui/editor-header.tsx
@@ -1,5 +1,4 @@
 import { Heading } from '@repo/ui-kit/components/heading'
-import { Separator } from '@repo/ui-kit/components/separator'
 import { ThemeSwitcher } from '@repo/ui-kit/components/theme'
 import type { FC } from 'react'
 import { EditorStatus } from './editor-status'
@@ -10,16 +9,15 @@ import { PlaybackTimer } from './playback-timer'
 export const EditorHeader: FC = () => {
   return (
     <div className="flex w-full items-center gap-2 border-b-2 bg-background p-2">
-      <Heading.Root>
+      <PlaybackButton />
+      <LoadExampleButton />
+      <PlaybackTimer />
+      <EditorStatus />
+      <div className="grow" />
+      <Heading.Root align="end">
         <Heading.SuperTitle>@kepvox/synth</Heading.SuperTitle>
         <Heading.Title>playground</Heading.Title>
       </Heading.Root>
-      <Separator orientation="vertical" />
-      <PlaybackButton />
-      <LoadExampleButton />
-      <EditorStatus />
-      <PlaybackTimer />
-      <div className="grow" />
       <ThemeSwitcher variant="outline" size="lg" />
     </div>
   )
